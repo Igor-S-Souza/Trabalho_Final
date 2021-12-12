2d8800aed6c758c50b8179439aa10d305b838647
Igor souza <89223876+lulamolusc0@users.noreply.github.com>
2021-12-12 19:15:42 -0300

    uga

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title></title>
+</head>
+<body>
+	<form  method="POST" action="menu.php">
+	<input  type="text" name="login"  placeholder=" " required>
+	<label>Login:  </label>
+
+	<br>
+
+	<input  type="password"  placeholder=" " name="senha" required>
+	<label>senha: </label>
+	<input type="submit" value="ACESSAR" >
+	</form>
+
+</body>
+</html>
---
+++ menu.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+   <body>
+      <br>
+      <br>
+      <br>
+      <center>
+               <!-- Abrir Solicitação -->
+               <a  href="abrir_solicitaçao">
+               <br> 
+               Abrir Solicitação          
+               </a>
+               <!-- Solicitações -->   
+               <a href="colaborador">
+               <br> 
+               Solicitações
+               </a>
+               <!-- Usuários -->
+               <a href="usuarios_por_setor">
+               <br>
+               Usuários
+               </a>
+               <!-- Andamento -->
+               <a href="solicitaçoes_status">
+               <br>
+               Andamento
+               </a>
+               <!-- Contatos -->
+               <a href="contato">
+               <br>
+               Contatos
+               </a>
+      </center>
+      <br>
+      <br>
+      <br>
+   </body>
+</html>
